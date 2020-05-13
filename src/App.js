d2ba5333ae708e1b98f6392ec7529ea11c11a3fc
faisalprady13 <faisalprady@gmail.com>
2020-05-13 16:08:35 +0200

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./assets/scss/main.scss";
 import LandingPage from "./components/LandingPage/LandingPage";
+import Navigation from "./components/Navigation/Navigation";
 
 //Choose the project from the "components" folder to see the example
 function App() {
   return (
     <div className="App">
+      <Navigation />
       <LandingPage />
     </div>
   );
