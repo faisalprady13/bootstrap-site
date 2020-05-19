b0464247aebfa521ebadf6cc6ad1147be39b843e
faisalprady13 <faisalprady@gmail.com>
2020-05-19 14:13:29 +0200

    add blob to background

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -2,11 +2,13 @@ import React from "react";
 import { Container, Row, Col, Image, Jumbotron, Button } from "react-bootstrap";
 import Cooking from "../../assets/img/LandingPage/cooking.svg";
 import { Fade } from "react-reveal";
+import Blob1 from "../../assets/img/LandingPage/blob-1.svg";
 
 const LandingPage = () => {
   return (
     <Jumbotron className="hero mb-0">
-      <Container>
+      <img src={Blob1} className="blob-1" alt="blob 1" />
+      <Container className="content">
         <Fade>
           <Row>
             <Col className="align-self-center text-center text-sm-center text-md-left">
