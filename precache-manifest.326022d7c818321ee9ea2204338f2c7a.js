9e0cc27d08e4242afdfbaed952aa7434ce40305a
faisalprady13 <faisalprady@gmail.com>
2020-05-24 20:53:50 +0200

    Updates

---
+++ precache-manifest.326022d7c818321ee9ea2204338f2c7a.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "881c595cfd848daa1b296e14e66a025a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b76d3a1c932b5bda7798",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "144c5ec0891a1d52b52b",
+    "url": "/static/css/main.59426219.chunk.css"
+  },
+  {
+    "revision": "b76d3a1c932b5bda7798",
+    "url": "/static/js/2.19b68ee9.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.19b68ee9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "144c5ec0891a1d52b52b",
+    "url": "/static/js/main.4b7e86f0.chunk.js"
+  },
+  {
+    "revision": "c193a06788038a102740",
+    "url": "/static/js/runtime-main.31f76470.js"
+  },
+  {
+    "revision": "53629fa9a1e94c6e7a7bee49efa044af",
+    "url": "/static/media/blob-1.53629fa9.svg"
+  },
+  {
+    "revision": "c24c5f02a19b815dafb644a4bb56d317",
+    "url": "/static/media/blob-1.c24c5f02.svg"
+  },
+  {
+    "revision": "a667eb874581c6e0db441a1896c34832",
+    "url": "/static/media/blob-2.a667eb87.svg"
+  },
+  {
+    "revision": "d346ca272d38b35cb4639a773a1f503d",
+    "url": "/static/media/blob-3.d346ca27.svg"
+  },
+  {
+    "revision": "00069e92f3db57f091eba8552c48f2ab",
+    "url": "/static/media/blob-4.00069e92.svg"
+  },
+  {
+    "revision": "adc4a19ce2e06ed855afc99bb2139755",
+    "url": "/static/media/cook.adc4a19c.jpg"
+  },
+  {
+    "revision": "ce14a3fe68fc4c5ea53c533f04524f46",
+    "url": "/static/media/cooking.ce14a3fe.jpg"
+  },
+  {
+    "revision": "ed4bce393d7ac39da31e4a5c2a09fa5e",
+    "url": "/static/media/cooking.ed4bce39.svg"
+  },
+  {
+    "revision": "ea5bb719d35c78cfd5746c18626bbee7",
+    "url": "/static/media/kitchen.ea5bb719.jpg"
+  },
+  {
+    "revision": "819dd86725a5f6d4cf384f70afb7056f",
+    "url": "/static/media/logo.819dd867.svg"
+  },
+  {
+    "revision": "29e89d4946715ea655f7f1f2cb2d1bc7",
+    "url": "/static/media/waveDown.29e89d49.svg"
+  }
+]);
